d4b5b3a48b510584300f7d130021dec969df5338
Collin <collin.caldwell@gmail.com>
2015-01-22 18:23:56 -0500

    fixed my name with correct spaces' :

--- M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -63,7 +63,7 @@ import edu.gatech.oad.antlab.pkg2.*;
 		  
                   //Person2 replace P2 with your name
 		  //and gburdell with your gt id
-		  Person2 p2 = new Person2("Collin");
+		  Person2 p2 = new Person2(" Collin Caldwell ");
 		  toPrint += p2.toString("ccaldwell9");
 		  
                   //Person3 replace P3 with your name
