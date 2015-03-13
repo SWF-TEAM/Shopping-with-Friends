2df8fc60e44bdbc78039aa76641384ef54ed4f6c
Michael Adkison <madkison3@gatech.edu>
2015-03-12 23:09:03 -0400

    replace hardcoded strings

--- app/src/main/java/am/te/myapplication/FriendDetails.java
+++ app/src/main/java/am/te/myapplication/FriendDetails.java
@@ -117,27 +117,6 @@ public class FriendDetails extends Activity {
                 return removeFriend();
                 //already in system
             }
-            /*
-            try {
-                // Simulate network access.
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                return false;
-            }
-
-            for (String credential : DUMMY_CREDENTIALS) {
-                String[] pieces = credential.split(":");
-                if (pieces[0].equals(mEmail)) {
-                    // Account exists, return true if the password matches.
-                    return pieces[1].equals(mPassword);
-                }
-            }
-
-            */
-
-            // Authentication with local list of registered users (will be replaced with database auth soon^(TM))
-            /*User userToAuthenticate = new User(mEmail, mPassword);
-            return RegistrationModel.getUsers().contains(userToAuthenticate);*/
         }
 
         protected boolean removeFriend() {
--- app/src/main/java/am/te/myapplication/Login.java
+++ app/src/main/java/am/te/myapplication/Login.java
@@ -238,28 +238,6 @@ public class Login extends Activity implements LoaderCallbacks<Cursor> {
             if (State.local) {
                 return RegistrationModel.getUsers().contains(userToAuthenticate);
             } else {
-                //attempt authentication against a network service.
-                /*
-                try {
-                    // Simulate network access.
-                    Thread.sleep(2000);
-                } catch (InterruptedException e) {
-                    return false;
-                }
-
-                for (String credential : DUMMY_CREDENTIALS) {
-                    String[] pieces = credential.split(":");
-                    if (pieces[0].equals(mUsername)) {
-                        // Account exists, return true if the password matches.
-                        return pieces[1].equals(mPassword);
-                    }
-                }
-
-                */
-
-                // Authentication with local list of registered users (will be replaced with database auth soon^(TM))
-                //User userToAuthenticate = new User(mUsername, mPassword);
-                //return RegistrationModel.getUsers().contains(userToAuthenticate);
                 String loginKey = getLoginKey();
                 if (!(loginKey.equals("*NOSUCHUSER") || loginKey.equals("") || loginKey == null)) {
                     uniqueIDofCurrentlyLoggedIn = loginKey;
