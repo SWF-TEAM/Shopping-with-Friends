3679b876c4d54e4736cf2ed651183133a9bafc0f
mmanguno <mmanguno3@gatech.edu>
2015-04-02 17:11:20 -0400

    Should install junit dependencies.

---
+++ app/src/main/java/am/te/myapplication/tests/MainActivityTest.java
@@ -0,0 +1,14 @@
+package am.te.myapplication.tests;
+
+import android.test.ActivityTestCase;
+
+import junit.framework.Assert;
+
+
+public class MainActivityTest extends ActivityTestCase {
+
+    public void testHappy(){
+        Assert.assertTrue(true);
+    }
+
+}
--- app/src/main/java/am/te/myapplication/tests/TaskTest.java
+++
@@ -1,6 +0,0 @@
-package am.te.myapplication.tests;
-
-public class TaskTest {
-
-    //I don't do anything
-}
